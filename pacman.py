a1c4e12bc3b8b4102c65b6075d8940fa29be628c
Elias <A01028482@tec.mx>
2022-03-24 12:50:59 -0600

    ghost #

--- pacman.py
+++ pacman.py
@@ -18,13 +18,12 @@ state = {'score': 0}
 path = Turtle(visible=False)
 writer = Turtle(visible=False)
 aim = vector(5, 0)
-pacman = vector(-40, -80)
+pacman = vector(-120, -80)
 ghosts = [
     [vector(-180, 160), vector(5, 0)],
     [vector(-180, -160), vector(0, 5)],
     [vector(100, 160), vector(0, -5)],
     [vector(100, -160), vector(-5, 0)],
-    [vector(-180, 160), vector(5,0)],
 ]
 tiles = [
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
